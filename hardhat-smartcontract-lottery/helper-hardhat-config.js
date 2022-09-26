033f4080141c47560aecfd2af266613649aa7616
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2022-09-26 18:39:43 +0530

    initial commit

---
+++ hardhat-smartcontract-lottery/helper-hardhat-config.js
@@ -0,0 +1,11 @@
+const networkConfig = {
+    4: {
+        name: "goerli",
+        subscriptionId: "6926",
+        gasLane: "0x79d3d8832d904592c0bf9818b621522c988bb8b0c05cdc3b15aea1b6e8db0c15", // 30 gwei
+        keepersUpdateInterval: "30",
+        raffleEntranceFee: "100000000000000000", // 0.1 ETH
+        callbackGasLimit: "500000", // 500,000 gas
+        vrfCoordinatorV2: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    },
+}
