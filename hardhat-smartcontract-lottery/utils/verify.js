5bcb858e7385329cb43ea032124023324b8c3f03
Shourya742 <84662239+Shourya742@users.noreply.github.com>
2022-09-27 07:10:54 +0530

    testing left

---
+++ hardhat-smartcontract-lottery/utils/verify.js
@@ -0,0 +1,16 @@
+const { run } = require("hardhat")
+const verify = async (contractAddress, args) => {
+    console.log("Verifyig contract...")
+    try {
+        await run("verify:verify", {
+            address: contractAddress,
+            constructorArguments: args,
+        })
+    } catch (e) {
+        if (e.message.toLowerCase().includes("already verified")) {
+        } else {
+            console.log(e)
+        }
+    }
+}
+module.exports = { verify }
